22aa8b08af43dcf4e3a02a57417f74fd24e5348c
dthtien <tien.dau@employmenthero.com>
2022-03-27 16:00:11 +0700

    chore(distinct_issue): implement code

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
+
+  scope :order_rating, -> { order(rating: :desc) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,26 @@ class User < ApplicationRecord
   include Publishable
 
   has_many :comments
+
+  class << self
+    def with_comments
+      joins(:comments)
+    end
+
+    def order_comment_rating
+      from(
+        select(%{
+          DISTINCT ON (users.id) users.*, comments.rating as highest_rating
+        }).with_comments,
+        :users
+      ).order(highest_rating: :desc)
+    end
+  end
 end
+
+# from(
+# with_latest_job.select(%{
+# DISTINCT ON (members.id) members.*, COALESCE(employment_histories.title, '') AS job_title
+# }), :members)
+# .order(job_title: options[:dir])
+
